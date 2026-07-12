3cbf02bf5ee8b4ba2444d3a9178e37d3948dd668
salearn-dev <dev@salearn.online>
2026-07-12 21:28:02 +0200

    Add deterministic match fixtures

---
+++ tests/fixtures/match.ts
@@ -0,0 +1,18 @@
+import type { ApsSubject } from "../../src/lib/aps";
+
+export const strongSubjectProfile: ApsSubject[] = [
+  { name: "Mathematics", mark: 82 },
+  { name: "English HL", mark: 74 },
+  { name: "Physical Sciences", mark: 69 },
+  { name: "Life Sciences", mark: 63 },
+  { name: "Geography", mark: 58 },
+  { name: "History", mark: 51 },
+  { name: "Business Studies", mark: 45 },
+  { name: "Life Orientation", mark: 99 },
+];
+
+export const literacyProfile: ApsSubject[] = [
+  { name: "Mathematical Literacy", mark: 71 },
+  { name: "English FAL", mark: 62 },
+  { name: "Life Sciences", mark: 59 },
+];
